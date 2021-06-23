0018cbee68316b26505b3423e02c1b5f69e43ab2
Pincet <pincetgore@mail.ru>
2021-06-23 23:03:17 +0300

    2 and 3 tasks done

--- main.cpp
+++ main.cpp
@@ -1,35 +1,90 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int iSqCycle (int arr[], int size);
+int CreateFiles (string name1, string name2);
 
 int main()
 {
-    //Task1
-    int* ptrArr;
-    int length;
-    cout << "Enter size of your array (positive integer): ";
-    cin >> length;
-        if (length > 0)
-        {
-            ptrArr = new int[length];
-            iSqCycle (ptrArr, length);
-            delete[] ptrArr;
-            ptrArr = nullptr;
-        }
-        else
-            cout << "Your value is not proper. Exit" << endl;
-return 0;
+//    //Task1
+//    int* ptrArr;
+//    int length;
+//    cout << "Enter size of your array (positive integer): ";
+//    cin >> length;
+//        if (length > 0)
+//        {
+//            ptrArr = new int[length];
+//            iSqCycle (ptrArr, length);
+//            delete[] ptrArr;
+//            ptrArr = nullptr;
+//        }
+//        else
+//            cout << "Your value is not proper. Exit" << endl;
+
+
+//    //Task2
+//    int** ptrArrMatrix;
+//    int m = 4;
+//    int n = 4;
+
+//    // array memory allocation
+//    ptrArrMatrix = new int * [m];
+//    for (int i = 0; i < m; i++) {
+//        ptrArrMatrix[i] = new int[n];
+//    }
+
+//    // array filling
+//    cout << "Your random array 4x4: " << endl;
+//    for (int i=0; i<m; i++){
+//        for (int j=0; j<n; j++){
+//            ptrArrMatrix[i][j]=arc4random();
+//            cout << ptrArrMatrix[i][j] << " ";
+//    }
+//        cout << endl;
+//   }
+
+//    // freing memory
+//    for (int i = 0; i < m; i++) {
+//    delete [] ptrArrMatrix[i];
+//    }
+//    delete[] ptrArrMatrix;
+//    ptrArrMatrix = nullptr;
+
+    //Task3
+    string name1, name2;
+    cout << "Enter the name of the first file: ";
+    cin >> name1;
+    name1 = name1+".txt";
+    cout << "Enter the name of the second file: ";
+    cin >> name2;
+    name2 = name2+".txt";
+    CreateFiles (name1, name2);
+
+    return 0;
+}
+
+int CreateFiles (string name1, string name2)
+{
+    ofstream f1out(name1);
+    f1out << "1. In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.";
+    f1out.close();
+
+    ofstream f2out(name2);
+    f2out <<"2. My whole class once got detention because I drew a penis with a glue stick on the whiteboard and when the teacher went to wipe off the board all the fluff came off and stuck to the glue. I never got in trouble for it because my whole class found it too funny to tell the teacher it was me.";
+    f2out.close();
+    return 0;
 }
 
 int iSqCycle (int arr[], int size)
 {
-for (int i=1; i<size; i++)
-{
-    arr[i]=pow(2,i);
-    cout << arr[i]<< endl;
-}
-return 0;
+    for (int i=1; i<size; i++)
+    {
+        arr[i]=pow(2,i);
+        cout << arr[i]<< endl;
+    }
+    return 0;
 }
